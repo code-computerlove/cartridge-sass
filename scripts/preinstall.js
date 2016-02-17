37e79e26dece7a03f2649c3d2fe292695d133c44
Barney Scott <barney.scott@codecomputerlove.com>
2016-02-17 12:08:10 +0000

    Slight refactor to move the full slate check code over to quarry

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -2,7 +2,4 @@
 
 var slateCli = require('slate-cli');
 
-if(!slateCli.hasSlate()) {
-	console.error('Slate is not set up in this directory. Please set it up first before installing this module');
-	process.exit(1);
-}
+slateCli.ensureSlate();
