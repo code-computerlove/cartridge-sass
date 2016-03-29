5cb8a3df86449a22a84f48015a68b6c0bfab61ed
tawashley <trisashley@gmail.com>
2016-03-29 15:44:26 +0100

    tests: Clean up directory, files

--- test/cartridgeSassSpec.js
+++ test/cartridgeSassSpec.js
@@ -1,3 +1,4 @@
+var spawn = require('child_process').spawn;
 var path = require('path');
 var chai = require('chai');
 var expect = chai.expect;
@@ -5,14 +6,34 @@ var expect = chai.expect;
 // chai.use(require('chai-fs'));
 chai.should();
 
+const MOCK_PROJECT_DIR = path.join(process.cwd(), 'test', 'mock-project');
+const STYLE_SRC_DIR = path.join(MOCK_PROJECT_DIR, '_source', 'styles');
+const STYLE_DEST_DIR = path.join(MOCK_PROJECT_DIR, 'public', '_client', 'styles');
+
+process.chdir(MOCK_PROJECT_DIR);
+
+function runGulpTask(options, callback) {
+
+    var gulp = spawn('gulp', options)
+
+    gulp.on('close', function() {
+        callback();
+    });
+
+}
+
 describe('As a user of the cartridge-sass module', function() {
+
+	this.timeout(10000);
 
 	describe('when `gulp sass` is run', function() {
 
+		before(function(done) {
+			runGulpTask(['sass'], done)
+		})
+
 		it('should generate the main.scss file', function() {
-
 			expect(true).to.be.true;
-
 		})
 
 	})
