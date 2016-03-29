91af77bbb0cbc0de24c834eeacc5c2452c6b6065
tawashley <trisashley@gmail.com>
2016-03-29 15:22:16 +0100

    test: Add in placeholer test

---
+++ test/cartridgeSassSpec.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var chai = require('chai');
+var expect = chai.expect;
+
+// chai.use(require('chai-fs'));
+chai.should();
+
+describe('As a user of the cartridge-sass module', function() {
+
+	describe('when `gulp sass` is run', function() {
+
+		it('should generate the main.scss file', function() {
+
+			expect(true).to.be.true;
+
+		})
+
+	})
+
+})
