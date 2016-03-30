0290e0dc64a082738b6bd68a571dd5a194de8116
tawashley <trisashley@gmail.com>
2016-03-30 12:06:37 +0100

    tests: Add tests for running gulp task with prod flag

--- test/cartridgeSassSpec.js
+++ test/cartridgeSassSpec.js
@@ -12,9 +12,17 @@ var MOCK_PROJECT_DIR = path.join(process.cwd(), 'test', 'mock-project');
 var STYLE_SRC_DIR = path.join(MOCK_PROJECT_DIR, '_source', 'styles');
 var STYLE_DEST_DIR = path.join(MOCK_PROJECT_DIR, 'public', '_client', 'styles');
 
+var MAIN_SCSS_FILEPATH = path.join(STYLE_SRC_DIR, 'main.scss');
+var MAIN_CSS_FILEPATH = path.join(STYLE_DEST_DIR, 'main.css');
+var MAIN_CSS_SOURCEMAP_FILEPATH = path.join(STYLE_DEST_DIR, 'main.css.map');
+
 process.chdir(MOCK_PROJECT_DIR);
 
 function runGulpTask(options, callback) {
+
+	fs.remove(MAIN_SCSS_FILEPATH);
+	fs.remove(MAIN_CSS_FILEPATH);
+	fs.remove(MAIN_CSS_SOURCEMAP_FILEPATH);
 
     var gulp = spawn('gulp', options)
 
@@ -28,28 +36,42 @@ describe('As a user of the cartridge-sass module', function() {
 
 	this.timeout(10000);
 
-	describe('when `gulp sass` is run', function() {
+	describe('when `gulp sass` is run WITHOUT production flag', function() {
 
 		before(function(done) {
 			runGulpTask(['sass'], done)
 		})
 
 		it('should generate the main.scss file in the _source dir', function() {
-			var mainScssFilePath = path.join(STYLE_SRC_DIR, 'main.scss');
-
-			expect(mainScssFilePath).to.be.a.file();
+			expect(MAIN_SCSS_FILEPATH).to.be.a.file();
 		})
 
 		it('should add the main.css file to the public styles folder', function() {
-			var mainCssFilePath = path.join(STYLE_DEST_DIR, 'main.css');
-
-			expect(mainCssFilePath).to.be.a.file();
+			expect(MAIN_CSS_FILEPATH).to.be.a.file();
 		})
 
 		it('should add the main.css.map sourcemap file to the public styles folder', function() {
-			var mainCssSourceMapFilePath = path.join(STYLE_DEST_DIR, 'main.css.map');
+			expect(MAIN_CSS_SOURCEMAP_FILEPATH).to.be.a.file();
+		})
 
-			expect(mainCssSourceMapFilePath).to.be.a.file();
+	})
+
+	describe('when `gulp sass` is run WITH production flag', function() {
+
+		before(function(done) {
+			runGulpTask(['sass', '--prod'], done)
+		})
+
+		it('should generate the main.scss file in the _source dir', function() {
+			expect(MAIN_SCSS_FILEPATH).to.be.a.file();
+		})
+
+		it('should add the main.css file to the public styles folder', function() {
+			expect(MAIN_CSS_FILEPATH).to.be.a.file();
+		})
+
+		it('should add the main.css.map sourcemap file to the public styles folder', function() {
+			expect(MAIN_CSS_SOURCEMAP_FILEPATH).to.be.a.file();
 		})
 
 	})
