5ef7ba8e774f94d457de8a79bb7c655c97f3b26c
tawashley <trisashley@gmail.com>
2016-06-27 20:49:28 +0100

    fix(task): Correctly add cssNano to post css plugins if no config object is provided

--- task.js
+++ task.js
@@ -64,7 +64,7 @@ module.exports = function(gulp, projectConfig, tasks) {
 				if(postCssConfig.cssNano) {
 					postCssPlugins.push(cssNano(postCssConfig.cssNano));
 				} else {
-					postCssPlugins.push();
+					postCssPlugins.push(cssNano());
 				}
 			}
 
