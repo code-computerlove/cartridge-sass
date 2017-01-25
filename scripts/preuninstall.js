2e02e709806748315604e68c6bb3a0c1c3b3c1db
Dave Aspinall <daveaasp@gmail.com>
2017-01-25 10:55:46 +0000

    feat(gulp): add stylelint to sass task

---
+++ scripts/preuninstall.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Package config
+var packageConfig = require('../package.json');
+
+// Node utils
+var cartridgeUtil = require('cartridge-module-util')(packageConfig);
+var path          = require('path');
+
+var TASK_NAME = 'sass';
+
+// Run through module uninstallation
+cartridgeUtil.removeFromRc(packageConfig.name)
+	.then(function runRemoveModuleConfig() {
+		return cartridgeUtil.removeModuleConfig(path.resolve('_config', 'task.' + TASK_NAME + '.js'));
+	})
+	.then(function runRemoveStylelintRc() {
+		return cartridgeUtil.removeFromProjectDir(['.stylelintrc']);
+	})
+	.then(cartridgeUtil.finishUninstall)
